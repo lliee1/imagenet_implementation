69b0fa097221705993122291b637ab910804c133
jee6006 <jee6006@naver.com>
2023-10-31 08:44:43 +0900

    fix : change val_transform

--- train.py
+++ train.py
@@ -38,7 +38,8 @@ train_transform = transforms.Compose([
 ])
 
 val_transform = transforms.Compose([
-    transforms.Resize(224),
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
